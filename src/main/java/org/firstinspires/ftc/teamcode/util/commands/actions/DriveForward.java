85f994a9b9ee4694b64608fe8a0f9f9d3646db79
Abdul <abdulgzia@gmail.com>
2025-10-25 13:34:02 -0400

    Reused a lot of code from last year because the drive system should be the same. It still needs testing though

---
+++ src/main/java/org/firstinspires/ftc/teamcode/util/commands/actions/DriveForward.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.util.commands.actions;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.util.Robot;
+import org.firstinspires.ftc.teamcode.util.commands.Command;
+
+// Uses encoders
+public class DriveForward implements Command {
+    private double distance;
+    private Robot robot;
+
+    public DriveForward(double inches, Robot robot) {
+        distance = inches*Robot.getCountsPerInchForDriveMotors();
+
+        // init robot
+        this.robot = robot;
+    }
+
+    public void init() {
+        this.robot.setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.robot.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.robot.setDriveTargetPosition((int)distance);
+    }
+    //    @Override
+    public void run() {
+        if(distance>=0) {
+            this.robot.setAllDrivePower(1.0);
+        } else {
+            this.robot.setAllDrivePower(-1.0);
+        }
+    }
+
+    public void shutdown() {
+        this.robot.setAllDrivePower(0.0);
+        this.robot.setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.robot.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    //    @Override
+    public boolean isDone() {
+        if(distance>=0) {
+            return robot.atDriveTargetPosition(true);
+        } else {
+            return robot.atDriveTargetPosition(false);
+        }
+    }
+}
