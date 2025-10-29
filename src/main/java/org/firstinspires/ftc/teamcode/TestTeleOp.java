098dc6d4179d566ac8021ddbce0ddf8f8a2f522a
ftc519 <ftc519@gmail.como>
2025-10-28 20:47:27 -0400

    Added TestTeleOp

---
+++ src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name = "TestTeleOp")
+public class TestTeleOp extends OpMode {
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
+
+    @Override
+    public void init() {
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "leftFront");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "rightFront");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "leftBack");
+        backRightMotor = hardwareMap.get(DcMotor.class, "rightBack");
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+
+        if (gamepad1.dpad_up) {
+            frontLeftMotor.setPower(1);
+            frontRightMotor.setPower(1);
+            backLeftMotor.setPower(1);
+            backRightMotor.setPower(1);
+        }
+        else if (gamepad1.dpad_down){
+            frontLeftMotor.setPower(-1);
+            frontRightMotor.setPower(-1);
+            backLeftMotor.setPower(-1);
+            backRightMotor.setPower(-1);
+        }
+        else if (gamepad1.dpad_left){
+            frontLeftMotor.setPower(-1);
+            frontRightMotor.setPower(1);
+            backLeftMotor.setPower(-1);
+            backRightMotor.setPower(1);
+        }
+    }
+
+}
