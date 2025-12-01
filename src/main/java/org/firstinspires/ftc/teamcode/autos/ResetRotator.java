5be9abf2ff011ef6449a9caf926985d63cdc0e18
ftc519 <ftc519@gmail.com>
2025-12-01 10:03:01 -0500

    Created new autonomous opmode for testing the alignRotator command, still needs Magnetic Limit Switches to be mounted and configured to work. -Matthew Wacek

---
+++ src/main/java/org/firstinspires/ftc/teamcode/autos/ResetRotator.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.util.OpModeBase;
+import org.firstinspires.ftc.teamcode.util.commands.Command;
+import org.firstinspires.ftc.teamcode.util.commands.actions.AlignRotator;
+
+@Autonomous(name="ResetRotator",group="Testing")
+public class ResetRotator extends OpModeBase {
+
+    Command cmd = new AlignRotator(robot);
+
+    private boolean finished = false;
+
+    public void init() {super.init();}
+
+    @Override
+    public void loop() {
+        if(!cmd.isDone()) {
+            cmd.run();
+        } else if(cmd.isDone() && !finished) {
+            cmd.shutdown();
+            finished = true;
+        }
+    }
+}
